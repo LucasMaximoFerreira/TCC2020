820373b519b7fc188d150ee50f4caa3cf364b80d
LucasMaximoFerreira <lucas.maximo.ferreira21@gmail.com>
2020-09-22 23:27:25 -0300

    login

--- TCC/app/src/main/java/com/example/tcc/Login.java
+++ TCC/app/src/main/java/com/example/tcc/Login.java
@@ -3,8 +3,13 @@ package com.example.tcc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    EditText txtLogin, txtSenha;
+    Button btnLogar, btnCadastrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- TCC/app/src/main/java/dao/conectarBD.java
+++ TCC/app/src/main/java/dao/conectarBD.java
@@ -3,10 +3,18 @@ package dao;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import model.cadastro_cliente;
 
 public class conectarBD extends AsyncTask<Integer, Object, Boolean> {
 
@@ -22,6 +30,32 @@ public class conectarBD extends AsyncTask<Integer, Object, Boolean> {
     //ARMAZENAR A OPPÇÃO DO USUARIO
     int op;
 
+
+    //variavel para armazenar o status do login
+    private Boolean login;
+
+    public Boolean getLogin() {
+        return login;
+    }
+
+    public void setLogin(Boolean login) {
+        this.login = login;
+    }
+    ////////////////////////////////////////////
+
+
+    //instancia classe cliente
+    private cadastro_cliente classeCli = new cadastro_cliente();
+
+    public cadastro_cliente getClasseCli() {
+        return classeCli;
+    }
+
+    public void setClasseCli(cadastro_cliente classeCli) {
+        this.classeCli = classeCli;
+
+    }
+    ////////////////////////////////////////////
 
 
     //METODO PARA CRIAR CONEXAO COM O MYSQL
@@ -53,7 +87,23 @@ public class conectarBD extends AsyncTask<Integer, Object, Boolean> {
 
     @Override
     protected Boolean doInBackground(Integer... integers) {
-        return null;
+        Boolean resp = null;
+
+        op = integers[0];
+
+        connect();
+
+        switch (op){
+            case 0:
+                resp = inserir();
+                break;
+            case 1:
+                resp = logar();
+                break;
+        }
+
+        return resp;
+
     }
 
     @Override
@@ -67,7 +117,83 @@ public class conectarBD extends AsyncTask<Integer, Object, Boolean> {
     @Override
     protected void onPostExecute(Boolean aBoolean) {
         super.onPostExecute(aBoolean);
+
+        switch (op) {
+            case 0:
+                if (aBoolean == true) {
+                    Toast.makeText(tela, "cadastro ok", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(tela, "erro", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 1:
+                if (aBoolean == false) {
+                    Toast.makeText(tela, "usuario nao cadastrado", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+        dialogo.dismiss();
+
+        disconnect();
     }
+    private Boolean logar(){
+        try{
+
+            String sql = "select * from cadastro_cliente where cpf_cli=? and senha_cli=?";
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setString(1, classeCli.getCpf_cli());
+            comando.setString(2, classeCli.getSenha_cli());
+            ResultSet tabelamemoria = comando.executeQuery();
+
+            if (tabelamemoria.next()) {
+                login = true;
+                return true;
+            } else {
+                login = false;
+                return false;
+            }
+
+        }catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+    private Boolean inserir(){
+        try{
+            SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+
+            Date dataUtil = formato.parse(classeCli.getDtnasc_cli());
+            java.sql.Date dataMYSQL = new java.sql.Date(dataUtil.getTime());
+
+            String sql = "insert into cadastro_cliente values (0,?,?,?,?,?,?,?)";
+            PreparedStatement comando = conexao.prepareStatement(sql);
+            comando.setString(1, classeCli.getNome_cli());
+            comando.setString(2, classeCli.getSenha_cli());
+            comando.setString(3, classeCli.getCpf_cli());
+            comando.setString(4, classeCli.getTel_cli());
+            comando.setString(5, classeCli.getCep_cli());
+            comando.setString(6, classeCli.getNum_cli());
+            comando.setString(7, classeCli.getComp_cli());
+            comando.setString(8, classeCli.getEmail_cli());
+            comando.setDate(9, dataMYSQL);
+            comando.setString(10, classeCli.getGen_cli());
+
+            comando.executeUpdate();
+
+            return true;
+
+        }catch (ParseException e){
+            e.printStackTrace();
+            return false;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+
+    }
+
 }
 
 //CRIAR CLASSE DE MODELO - INSERIR, LOGIN ENTRE OUTRAS
